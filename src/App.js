d04ba8f99435a3947082323fd582caededbf92ca
danielsafari143 <danielsafari143@gmail.com>
2023-09-20 01:42:45 +0200

    ADD ROUTER && REDUX LOGIC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Greeting from './features/greeting/Greeting';
 
 function App() {
   return (
    <Provider store={store}>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={Greeting}/>
+        </Routes>
+      </BrowserRouter>
    </Provider>
   );
 }
--- src/features/greeting/Greeting.js
+++ src/features/greeting/Greeting.js
@@ -1,3 +1,19 @@
-export default function Greeting() {
+import { useDispatch , useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchGreetings } from "./greetingSlice";
 
+
+export default function Greeting() {
+    const dispatch = useDispatch();
+    const greeting = useSelector(state => state.greeting.value)
+    
+    useEffect(() => {
+        dispatch(fetchGreetings())
+    } , [dispatch]);
+
+    return(<div>
+        <h2>
+            {greeting}
+        </h2>
+    </div>)
 }
